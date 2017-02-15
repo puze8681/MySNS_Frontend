b601314bdab2460272b053531eee5e0de4c0ef36
puze8681 <rsef0601@naver.com>
2017-02-16 02:06:36 +0900

    login_xml, register_xml

---
+++ app/src/main/java/com/example/parktaejun/mysns/JSONService.java
@@ -0,0 +1,8 @@
+package com.example.parktaejun.mysns;
+
+/**
+ * Created by parktaejun on 2017. 2. 16..
+ */
+
+public class JSONService {
+}
--- app/src/main/java/com/example/parktaejun/mysns/LoginActivity.java
+++ app/src/main/java/com/example/parktaejun/mysns/LoginActivity.java
@@ -1,13 +1,38 @@
 package com.example.parktaejun.mysns;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText login_id;
+    private EditText login_pw;
+
+    private Button login_btn;
+    private Button register_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login_id = (EditText)findViewById(R.id.login_id);
+        login_pw = (EditText)findViewById(R.id.login_pw);
+
+        login_btn = (Button)findViewById(R.id.login_btn);
+        register_btn = (Button)findViewById(R.id.register_btn);
+
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }
